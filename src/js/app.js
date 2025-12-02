73d5ba0ca0f6094c100cad1915805f068d6990ab
marie charlotte <charlottedoulcet90@gmail.com>
2025-12-02 15:08:43 +0100

    feat(api): implement fetchTodayFact to retrieve today's fact from the API

--- src/js/app.js
+++ src/js/app.js
@@ -11,3 +11,17 @@ export const fetchRandomFact = async () => {
     throw error;
   }
 };
+
+export const fetchTodayFact = async () => {
+  try {
+    const response = await fetch("https://uselessfacts.jsph.pl/api/v2/facts/today?language=en");
+    if (!response.ok) {
+      throw new Error(`API responded with status ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error while fetching ramdom fact:", error);
+    throw error;
+  }
+};

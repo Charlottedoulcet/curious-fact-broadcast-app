4674d19c4529ec3d4323d7330fdc4dc8bb227af2
marie charlotte <charlottedoulcet90@gmail.com>
2025-12-02 14:58:52 +0100

    feat(api): implement fetchRandomFact to retrieve a random fact from the API

---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+export const fetchRandomFact = async () => {
+  try {
+    const response = await fetch("https://uselessfacts.jsph.pl/api/v2/facts/random?language=en");
+    if (!response.ok) {
+      throw new Error(`API responded with status ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error while fetching ramdom fact:", error);
+    throw error;
+  }
+};
